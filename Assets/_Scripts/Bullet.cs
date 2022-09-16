8efe6cd57488f3d11be2489811a265459c5e7ca7
Daniel <danil.e@ironwill-et.com>
2022-09-16 13:55:37 +0300

    Additions to the boundary logic

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -6,15 +6,26 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D;
-    [SerializeField] private float _bulletSpeed;
+    [SerializeField] private float _bulletSpeed; //TODO: put into model
 
     public void Project(Vector2 objectDirection)
     {
         _rigidbody2D.AddForce(objectDirection * _bulletSpeed);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (col.gameObject.GetComponent<Boundary>())
+        {
+            print("heh");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.GetComponentInParent<Boundary>())
+        {
+            Destroy(gameObject);
+        }
     }
 }
