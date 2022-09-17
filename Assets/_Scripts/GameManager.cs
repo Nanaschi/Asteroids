d36492ff2aa8c66f6eb4c38da55ab5bce235428f
Daniel <danil.e@ironwill-et.com>
2022-09-17 08:30:19 +0300

    Created the explosion

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -8,13 +8,14 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private int _lives;
     [SerializeField] private int _respawnTime;
+    [SerializeField] private int _timeOfInvincibility; //TODO: put it into the player
 
     public void PlayerDied()
     {
         _lives--;
 
         if (_lives <= 0) GameOver();
-        else Invoke(nameof(Respawn), _respawnTime);
+        else StartCoroutine(Respawn());
     }
 
     private void GameOver()
@@ -22,10 +23,14 @@ public class GameManager : MonoBehaviour
         print("GameOver");
     }
 
-    public void Respawn()
+    private IEnumerator Respawn()
     {
+        yield return new WaitForSeconds(_respawnTime);
         _player.transform.position = Vector3.zero;
-        
+        var boxCollider2D = _player.GetComponent<BoxCollider2D>();
+        boxCollider2D.enabled = false;
         _player.gameObject.SetActive(true);
+        yield return new WaitForSeconds(_timeOfInvincibility);
+        boxCollider2D.enabled = true;
     }
 }
